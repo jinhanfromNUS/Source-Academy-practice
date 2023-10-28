89b47aa84386ee5458c35b68c2f4199b10079c73
jinhanfromNUS <111408810+jinhanfromNUS@users.noreply.github.com>
2023-10-28 15:25:39 +0800

    Update creating list (self-wriiten).js

--- creating list (self-wriiten).js
+++ creating list (self-wriiten).js
@@ -41,7 +41,22 @@ function enum_list(start, end) {
     return start === end
            ? list(start)
            : start < end
-           ? (start < end ? pair (start, building_list(start + 1, end)) : null)
-           : (start > end ? pair (start, building_list(start - 1, end )) : null);
+           ? (start < end ? pair (start, enum_list(start + 1, end)) : null)
+           : (start > end ? pair (start, enum_list(start - 1, end )) : null);
 }
 
+function sort_two_lists(first_list, second_list) {
+    if (first_list === null && second_list === null) { return null; }
+    if (first_list === null) { return second_list; }
+    if (second_list === null) { return first_list; }
+    
+    if (head(first_list) <= head(second_list)) {
+        return pair(head(first_list), sort_two_lists(tail(first_list), second_list));
+    } else if (head(first_list) > head(second_list)) {
+        return pair(head(second_list), sort_two_lists(first_list, tail(second_list)));
+    } else {
+        return null;
+    }
+}
+
+sort_two_lists([1, [3, [5, null]]], [2, [3, [6, null]]]);
